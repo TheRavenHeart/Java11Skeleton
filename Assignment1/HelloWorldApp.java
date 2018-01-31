81de16d4f0842d852a7963c82e75097c80f0fcca
TheRavenHeart <36009501+TheRavenHeart@users.github.com>
2018-01-31 11:30:55 -0800

    I wrote programs

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,16 @@
 // Student name:
 // This is where your first assignment goes!
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Cat");
+        System.out.println("Probopass");
+        System.out.println("10+10=20");
+        System.out.println("It is belived that Lee Harvey Oswald killed JFK");
+        System.out.println("Denmark is most likely a real country");
+        System.out.println("2018-01-31");
+    }
+}

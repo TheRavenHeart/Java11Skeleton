161d8b7e434afc56e7cea33c7281f59f9ad03a89
TheRavenHeart <36009501+TheRavenHeart@users.noreply.github.com>
2018-03-07 11:34:35 -0800

    Necrodancer

--- Assignment6/AnimalFeature.java
+++ Assignment6/AnimalFeature.java
@@ -1,8 +1,16 @@
-
+import java.util.Scanner;
 class AnimalFeature{
-    {
-        System.out.println("");
-        
-    }
+public static void main(String[] args) {
+    String name, feature, animal;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter the name of someone.");
+    name = sc.nextLine();
+    System.out.println("Please enter a feature.");
+    feature = sc.nextLine();
+    System.out.println("Please enter an animal.");
+    animal = sc.nextLine();
+    System.out.format("%s has %s like a %s.", name, feature, animal);
+    // This seems unnessesary and somewhat clunky compared to the other method as well as confusing.
+}
 }
 

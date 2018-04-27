dfc8dd7e6f48b2b09f3fa6f2ba6dea11146a71e0
TheRavenHeart <36009501+TheRavenHeart@users.github.com>
2018-04-27 11:11:43 -0700

    NOTES

--- Assignment11/InfoPrinted.java
+++ Assignment11/InfoPrinted.java
@@ -4,15 +4,15 @@
         Scanner sc = new Scanner(System.in);
         String name, Address, city, province, postal_code;
         System.out.println("Please enter your name.");
-        name = sc.nextLine();
+        name = sc.nextLine(); // take your name
         System.out.println("Please enter your address.");
-        Address = sc.nextLine();
+        Address = sc.nextLine(); // takes your address
         System.out.println("Please enter what city you live in.");
-        city = sc.nextLine();
+        city = sc.nextLine(); // take what city you live in
         System.out.println("Please enter what province you live in.");
-        province = sc.nextLine();
+        province = sc.nextLine(); // takes what province you live in
         System.out.println("Please enter your postal code.");
-        postal_code = sc.nextLine();
-        System.out.println(name + ", " + city + ", " + Address + ", " + city + ", " + province + ", " + postal_code + ".");
+        postal_code = sc.nextLine(); // takes your postal code
+        System.out.println(name + ", " + city + ", " + Address + ", " + city + ", " + province + ", " + postal_code + "."); // prints it all out
     }
 }

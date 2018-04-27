87d01cd7a2cbd163a8b228582064d548392e1c49
TheRavenHeart <36009501+TheRavenHeart@users.github.com>
2018-04-27 11:41:13 -0700

    12

--- Assignment12/GetNumbersFromFile.java
+++ Assignment12/GetNumbersFromFile.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 class GetNumbersFromFile {
     public static void main(String[] args) throws IOException {
-        File file = new File("data.txt");
+        File file = new File("data.txt"); // takes all the numbers
         Scanner scan = new Scanner(file);
         int age = 0;
         int totalpeople = 0;
@@ -12,8 +12,8 @@ class GetNumbersFromFile {
         Female = 0;
         AIQ = 0;
         AH = 0;
-        AgeY = Integer.MAX_VALUE;
-        ageO = 0;
+        AgeY = Integer.MAX_VALUE; // for the youngest
+        ageO = 0; // forthe oldest
         int MSIQO = 0;
         while (scan.hasNextInt()) {
             age = scan.nextInt();
@@ -23,17 +23,17 @@ class GetNumbersFromFile {
             IQ = scan.nextInt();
             height = scan.nextInt();
             Gender = scan.nextInt();
-            totalpeople ++;
+            totalpeople ++; // adds to the total people
             AIQ = AIQ + IQ;
             AH = AH + height;
            
-            if(age > ageO){
+            if(age > ageO){ // makes it that the hgihest age is stored
                 ageO = age;
             }
-            if (age < ageO && AgeY > age){
+            if (age < ageO && AgeY > age){ // mkeas it so that the youngest age is stored
                 AgeY = age;
             }
-            if(height <= 72 && Gender == 1 && IQ > 140){
+            if(height <= 72 && Gender == 1 && IQ > 140){ // sees whos over 6 feet a male and has an IQ over 140
                 MSIQO ++;
             }
             if(Gender == 1){
@@ -50,7 +50,7 @@ class GetNumbersFromFile {
         System.out.println("Females: " + Female);
         System.out.println("Males over 6 feet and greater than 140 IQ: " + MSIQO);
         System.out.println("Average IQ: " + AIQ);
-        System.out.println("Average height: " + AH);
+        System.out.println("Average height: " + AH); // prints it all out
         System.out.println("Oldest: " + ageO);
         System.out.println("Youngest: " + AgeY);
         scan.close();       //System.out.println(data);
--- Assignment12/PrintNumbersToFile.java
+++ Assignment12/PrintNumbersToFile.java
@@ -7,21 +7,20 @@ class PrintNumbersToFile {
         int dataA = 0;
         int Gender, IQ, Height;
         File file = new File("data.txt");
-        PrintStream print = new PrintStream(file);
+        PrintStream print = new PrintStream(file); // this prints it to the file
 
-        while (dataA != -1) {
+        while (dataA != -1) { // this makes it so that until age = -1 it keeps looping
             System.out.print("Age in years(-1 to exit): ");
             dataA = scan.nextInt();
             print.println(dataA);
-            if(dataA == -1){
+            if(dataA == -1){//checks if age = -1
                 continue;
             }
             System.out.println("Enter your IQ score");
             IQ = scan.nextInt();
             print.println(IQ);
             System.out.println("Please enter your preferred gender, 1 for male 0 for female.");
-            Gender = scan.nextInt();
-            print.println(Gender);
+            Gender = scan.nextInt();        //takes in all of the data
             print.println(Gender);
             System.out.println("Please enter your height in inches.");
             Height = scan.nextInt();

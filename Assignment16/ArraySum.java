c56c8aec3db320eec1b211d8e99902f65827bb91
TheRavenHeart <36009501+TheRavenHeart@users.github.com>
2018-05-16 11:54:01 -0700

    EVEN MORE NOTES

--- Assignment16/ArraySum.java
+++ Assignment16/ArraySum.java
@@ -2,9 +2,9 @@ class ArraySum {
     public static void main(String[] args) {
         int[] val = { 0, 1, 2, 3 };
         int sum;
-        sum = val[0] + val[1] + val[2] + val[3];
+        sum = val[0] + val[1] + val[2] + val[3]; // adds all the values
 
-        System.out.println("Sum of all numbers = " + sum);
+        System.out.println("Sum of all numbers = " + sum); // prints them out
 
     }
 }
--- Assignment16/ReverseArray.java
+++ Assignment16/ReverseArray.java
@@ -3,15 +3,15 @@ class ReverseArray {
         int[] val = { 0, 1, 2, 3 };
         int temp;
 
-        System.out.println("Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
+        System.out.println("Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3]); // prints the orginal order
 
-                        // reverse the order of the numbers in the array\\
+                        // reverses the order of the numbers in the array
         temp = val[0];
         val[0] = val[3];
         val[3] = temp;
         temp = val[2];
         val[2] = val[1];
         val[1] = temp;
-        System.out.println("Reversed Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
+        System.out.println("Reversed Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3]); // prints the reverse order
     }
 }
